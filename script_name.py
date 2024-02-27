c190eb9bae7b5d01f10f02d0c61e5924c3df9a2e
Aravind Reveendran <37244926+errwnd@users.noreply.github.com>
2024-02-27 12:40:06 +0530

    Update script_name.py

--- script_name.py
+++ script_name.py
@@ -1,8 +1,7 @@
-import sklearn
+
 import streamlit as st
 import numpy as np
-import pandas as pd
-from sklearn.linear_model import LogisticRegression
+import pandas as pd 
 import joblib  # Import joblib for loading the model
 
 # Load the pre-trained logistic regression model
